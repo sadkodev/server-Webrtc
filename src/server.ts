b345eec9c9358520957779bacb322197249f122f
sadkodev <kristiankastillo0@gmail.com>
2024-04-21 20:36:40 -0400

    ci(server): support web-socket

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import type { WebRTC } from "@/models/interface";
+import WebSocket from "ws";
 import { showMsg } from "./utils/errors";
+
+const port = 8181;
 
 export default class Server implements WebRTC {
   constructor() {}
 
   start() {
     showMsg({ msg: "Server started", type: "info" });
+    startServer(port);
   }
   stop(): void {}
 
@@ -13,3 +17,17 @@ export default class Server implements WebRTC {
 
   receive(data: string): void {}
 }
+
+const startServer = (port: number) => {
+  const wss = new WebSocket.Server({ port });
+  wss.on("connection", (ws) => {
+    ws.on("message", (message) => {
+      wss.clients.forEach((client) => {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(message.toString());
+          showMsg({ msg: message.toString(), type: "info" });
+        }
+      });
+    });
+  });
+};
